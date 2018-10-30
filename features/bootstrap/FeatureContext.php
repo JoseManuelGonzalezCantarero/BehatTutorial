f0bbde96c4abbba956f12353abcf2c0b8b244cd5
Manu Gonzalez <jgonzalezcantarero@gmail.com>
2018-10-30 16:35:19 +0100

    Behat hooks background chapter

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -2,9 +2,9 @@
 
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
 use Behat\MinkExtension\Context\MinkContext;
+
+require_once __DIR__.'/../../vendor/phpunit/phpunit/src/Framework/Assert/Functions.php';
 
 /**
  * Defines application features from the specific context.
@@ -22,6 +22,8 @@ class FeatureContext extends MinkContext implements Context, SnippetAcceptingCon
      */
     public function __construct()
     {
+        mkdir('test');
+        chdir('test');
     }
 
     /**
@@ -45,8 +47,41 @@ class FeatureContext extends MinkContext implements Context, SnippetAcceptingCon
      */
     public function iShouldSeeInTheOutput($string)
     {
-        if (strpos($this->output, $string) === false) {
-            throw new \Exception(sprintf('Did not see "%s" in output "%s"', $string, $this->output));
+        assertContains(
+            $string,
+            $this->output,
+            sprintf('Did not see "%s" in output "%s"', $string, $this->output)
+        );
+    }
+
+    /**
+     * @AfterScenario
+     */
+    public function moveOutOfTestDir()
+    {
+        chdir('..');
+        if (is_dir('test')) {
+            system('rm -r '.realpath('test'));
         }
     }
+
+    /**
+     * @BeforeScenario
+     */
+    public function moveIntoTestDir()
+    {
+            if (!is_dir('test')) {
+                mkdir('test');
+            }
+            chdir('test');
+    }
+
+    /**
+     * @Given I have a dir named :dir
+     */
+    public function iHaveADirNamed($dir)
+    {
+        mkdir($dir);
+    }
+
 }
