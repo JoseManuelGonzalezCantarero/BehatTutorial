450f951e14a478b8bf88f876adfae5cd1f10bca3
Manu Gonzalez <jgonzalezcantarero@gmail.com>
2018-10-30 17:43:38 +0100

    Finding elements by css and name chapter

--- mink.php
+++ mink.php
@@ -1,18 +1,31 @@
 <?php
 
-use Behat\Mink\Driver\GoutteDriver;
+use Behat\Mink\Driver\Selenium2Driver;
 use Behat\Mink\Session;
 
 require __DIR__.'/vendor/autoload.php';
 
-$driver = new GoutteDriver();
+$driver = new Selenium2Driver('chrome');
 $session = new Session($driver);
+$session->start();
 
 $session->visit('http://jurassicpark.wikia.com');
-
-echo "Status code: ".$session->getStatusCode() . "\n";
 echo "Current URL: ".$session->getCurrentUrl() . "\n";
-
 
 $page = $session->getPage();
 echo "First 75 chars: ".substr($page->getText() , 0, 75) . "\n";
+
+$header = $page->find('css', '.wds-community-header__sitename a');
+echo "The wiki site name is: ". $header->getText() . "\n";
+
+$subNav = $page->find('css', '.wds-tabs');
+$linkEl = $subNav->find('css', 'li a');
+echo "The link text is: ". $linkEl->getText() . "\n";
+
+$linkEl = $page->findLink('Books');
+echo "The link href is: ". $linkEl->getAttribute('href') . "\n";
+
+$linkEl->click();
+echo "Page URL after click: ". $session->getCurrentUrl() . "\n";
+
+$session->stop();
